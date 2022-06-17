c2726abbd478da62941f060a690bb971cc1963e6
leonid <kotovpetia@gmail.com>
2022-06-17 23:54:10 +0300

    get employees

--- src/main/java/org/spring/rest/controller/MyRestController.java
+++ src/main/java/org/spring/rest/controller/MyRestController.java
@@ -1,7 +1,29 @@
 package org.spring.rest.controller;
 
+import org.spring.rest.entity.Employee;
+import org.spring.rest.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/api")
 public class MyRestController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping("/employees")
+    public List<Employee> showAllEmployees() {
+        return employeeService.getAllEmployees();
+    }
+
+    @GetMapping("/employees/{id}")
+    public Employee getEmployee(@PathVariable int id) {
+        return employeeService.getEmployee(id);
+    }
 }
