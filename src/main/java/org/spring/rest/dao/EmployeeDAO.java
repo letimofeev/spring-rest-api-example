75f168e480c672488f67178b229227b5b61700dc
leonid <kotovpetia@gmail.com>
2022-06-17 22:49:54 +0300

    dao + service added

---
+++ src/main/java/org/spring/rest/dao/EmployeeDAO.java
@@ -0,0 +1,16 @@
+package org.spring.rest.dao;
+
+import org.spring.rest.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+
+    List<Employee> getAllEmployees();
+
+    void saveEmployee(Employee employee);
+
+    Employee getEmployee(int id);
+
+    void deleteEmployee(int id);
+}
---
+++ src/main/java/org/spring/rest/service/EmployeeService.java
@@ -0,0 +1,16 @@
+package org.spring.rest.service;
+
+import org.spring.rest.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    List<Employee> getAllEmployees();
+
+    void saveEmployee(Employee employee);
+
+    Employee getEmployee(int id);
+
+    void deleteEmployee(int id);
+}
